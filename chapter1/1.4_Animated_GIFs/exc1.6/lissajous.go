c710ed3bc0ceb3f2e205a94b29bbeb547314a741
aliqasemi <qasemi4245qq@gmail.com>
2022-08-16 01:32:45 +0430

    add exc 1.5 && 1.6

---
+++ chapter1/1.4_Animated_GIFs/exc1.6/lissajous.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+)
+
+var palette = []color.Color{
+	color.White,
+	color.Black,
+	color.RGBA{R: 0, G: 255, B: 0, A: 1},
+	color.RGBA{R: 0, G: 0, B: 255, A: 1},
+	color.RGBA{R: 255, G: 0, B: 0, A: 1},
+}
+
+func main() {
+	// go build lissajous.go
+	// lissajous.go > out.gif
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), uint8(int(rand.Uint32())%len(palette)))
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
